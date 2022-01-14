ab6a4098e84c0678e6b7a5ba0d299662a9bab54b
devmateusramos <devmateusramos@gmail.com>
2022-01-14 19:16:05 -0300

    commit 1.1 variaveis

---
+++ 1.1/src/Programa.java
@@ -0,0 +1,10 @@
+public class Programa {
+    public static void main(String[] args) {
+        int inteiros = 23 / 2;
+        System.out.println(inteiros);
+        double doubleSemPonto = 23 / 2;
+        System.out.println(doubleSemPonto);
+        double doubleComPonto = 23.0 / 2;
+        System.out.println(doubleComPonto);
+    }
+}
