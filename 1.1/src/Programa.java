cb2285e2b23237adfd836918769ee8b580894b2f
devmateusramos <devmateusramos@gmail.com>
2022-01-14 19:30:40 -0300

    more things

--- 1.1/src/Programa.java
+++ 1.1/src/Programa.java
@@ -6,5 +6,16 @@ public class Programa {
         System.out.println(doubleSemPonto);
         double doubleComPonto = 23.0 / 2;
         System.out.println(doubleComPonto);
+        double valorDouble = 123.5;
+        int doubleConvertido = (int) valorDouble;
+        System.out.println(doubleConvertido);
+        double decimalGrande = 123.45678;
+        System.out.println(decimalGrande);
+        long numeroGrande = 43124234234L;
+        System.out.println(numeroGrande);
+        short numeroPequeno = 2131;
+        System.out.println(numeroPequeno);
+        byte numeroNanico = 127;
+        System.out.println(numeroNanico);
     }
 }
